dbb97ff3e27b47f14fbf99345fcfdf394f13f856
Prayogabrd Ⓥ <72582850+P677hl@users.noreply.github.com>
2021-06-26 06:22:38 +0700

    Update bahan.sh

--- bahan.sh
+++ bahan.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/sh
 
 apt update && apt upgrade
+pkg install php -y
 pkg install git -y
 pkg install w3m -y
 pkg install lynx -y
 pkg install python -y
+pkg install python2 -y
 pkg install toilet -y
 exit
